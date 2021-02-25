eb85a99b3e0089e1be4c420179f8592159c8a976
knowbee <knowbeeinc@gmail.com>
2021-02-25 18:03:31 +0200

    create chat table

--- src/database/db.js
+++ src/database/db.js
@@ -3,10 +3,10 @@ import pg from "pg";
 
 const { NODE_ENV } = process.env;
 const env =
-  NODE_ENV === "test" || NODE_ENV === "dev" ? `${NODE_ENV}`.toUpperCase() : "";
+  NODE_ENV === "test" || NODE_ENV === "dev" ? `_${NODE_ENV}`.toUpperCase() : "";
 
 const config = {
-  connectionString: process.env[`DATABASE_URL_${env}`],
+  connectionString: process.env[`DATABASE_URL${env}`],
   max: 10,
   idleTimeoutMillis: 30000,
 };
@@ -14,15 +14,18 @@ const config = {
 const pool = new pg.Pool(config);
 
 pool.on("connect", () => {
-  console.log("connected");
+  console.log("connected to the Database");
 });
 
 const dropTables = () => {
+  const chatsTable = "DROP TABLE IF EXISTS chats";
+
   const usersTable = "DROP TABLE IF EXISTS users";
-  const dropTablesQuery = `${usersTable};`;
+
+  const dropTablesQueries = `${chatsTable}; ${usersTable};`;
 
   pool
-    .query(dropTablesQuery)
+    .query(dropTablesQueries)
     .then((res) => {
       pool.end();
     })
@@ -40,14 +43,23 @@ const createTables = () => {
       users(
         id SERIAL PRIMARY KEY,
         name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
         joined_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
       )`;
-  const createTablesQuery = `${usersTable};`;
+
+  const chatsTable = `CREATE TABLE IF NOT EXISTS
+      chats(
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        created_by INTEGER NOT NULL REFERENCES users (id),
+        participant INTEGER NOT NULL REFERENCES users (id),
+        "createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        )`;
+  const createTablesQueries = `${usersTable}; ${chatsTable}`;
 
   pool
-    .query(createTablesQuery)
+    .query(createTablesQueries)
     .then((res) => {
       pool.end();
     })
