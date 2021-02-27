9227ee412bfa23e90b15856c51b0b44e1703503e
knowbee <knowbeeinc@gmail.com>
2021-02-27 19:51:32 +0200

    add support for sockets

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import dotenv from "dotenv";
 import server from "./routes";
 import { logger } from "./helpers";
+import socketIo from "./sockets/socketio";
 
 dotenv.config();
 
@@ -12,5 +13,7 @@ const port = process.env.PORT || 5000;
 server.listen(port, hostname, () => {
   logger.info(`Server running at http://${hostname}:${port}/`);
 });
+
+socketIo(server);
 
 export default server;
