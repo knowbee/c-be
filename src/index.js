47d0321dc6f7ea44c73ec5aae4a66a2156615392
knowbee <knowbeeinc@gmail.com>
2021-02-27 08:37:13 +0200

    port

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const hostname = "127.0.0.1";
 
-const port = process.env.PORT || "5000";
+const port = process.env.PORT || 5000;
 
 server.listen(port, hostname, () => {
   logger.info(`Server running at http://${hostname}:${port}/`);
