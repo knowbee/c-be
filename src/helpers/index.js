b5c0cd412fedd6ef60fb0a33c704e6ecd2ac78bc
knowbee <knowbeeinc@gmail.com>
2021-03-02 20:28:04 +0200

    replace logger with morgan

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,5 @@
 import db from "../database/index";
 import jwt from "jsonwebtoken";
-import winston from "winston";
 import "dotenv/config";
 
 import {
@@ -45,31 +44,4 @@ const loggedInUser = async (req, res) => {
   }
 };
 
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "combined.log" }),
-  ],
-});
-
-//
-// If not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    })
-  );
-}
-
-export default logger;
-
-export { decodeToken, loggedInUser, logger };
+export { decodeToken, loggedInUser };
